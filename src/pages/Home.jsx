150648674b02c29660758cb645d7a910a3eabfdb
olivia-rgb <oliviaonyinyechiokafor@gmail.com>
2024-07-13 16:21:19 -0700

    another change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import Header from "../components/Header"
-import BrowseCar from "../components/UI/BrowseCar" 
-import PremiumBrands from "../components/UI/PremiumBrands"
-import FeaturedVehicle from '../components/UI/FeaturedVehicle'
-import Figures from "../components/UI/Figures"
-import SellingCar from "../components/UI/SellingCar"
-import AllVehicles from "../components/UI/AllVehicles"
-import WhyUs from "../components/UI/WhyUs"
-import Review from "../components/UI/Review"
-import Footer from "../components/Footer"
-
+import React from 'react';
+import Header from "../components/Header";
+import BrowseCar from "../components/UI/BrowseCar"; 
+import PremiumBrands from "../components/UI/PremiumBrands";
+import FeaturedVehicle from '../components/UI/FeaturedVehicle';
+import Figures from "../components/UI/Figures";
+import SellingCar from "../components/UI/SellingCar";
+import AllVehicles from "../components/UI/AllVehicles";
+import WhyUs from "../components/UI/WhyUs";
+import Review from "../components/UI/Review";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
-    <div className='min-h-screen flex flex-col justify-center items-center'>
-    <Header />
-    <BrowseCar />
-    <PremiumBrands />
-    <FeaturedVehicle />
-    <Figures />
-    <SellingCar />
-    <AllVehicles />
-    <WhyUs />
-    <Review />
-    <Footer />
+    <div className='min-h-screen flex flex-col items-center'>
+      <Header />
+      <div className='w-full max-w-7xl px-4'>
+        <BrowseCar />
+        <PremiumBrands />
+        <FeaturedVehicle />
+        <Figures />
+        <SellingCar />
+        <AllVehicles />
+        <WhyUs />
+        <Review />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
