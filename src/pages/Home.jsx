086781e06d0d309183ae4642825c27fa81e76180
olivia-rgb <oliviaonyinyechiokafor@gmail.com>
2024-09-06 14:51:22 +0100

    god abeg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,8 +42,8 @@ function Home() {
       <Header />
       <div className='w-full'>
         <TrendingAds />
-        <BrowseCar />
         <PremiumBrands />
+        <BrowseCar />
         <SellCar />
         <Figures />
         <AllVehicle />
